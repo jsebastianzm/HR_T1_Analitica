73e30731a0a8542a8a5769cae4bf99e788b491e9
Juan José Acevedo <84612450+juanjacevedo@users.noreply.github.com>
2023-08-25 17:52:53 -0500

    Primera parte del agregado de la función creada al Notebook

--- a_funciones.py
+++ a_funciones.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
-
+### Función para conocer información de cada una de las variables del dataframe
 def info_columns(df):
     for i in df.columns:
         print(i, df[i].unique())
-
+    print("Tamaño del DataFrame:",df.shape)
