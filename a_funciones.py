d7a0375bd508e19b99bf4ebb6da1121249a85dca
jsebastianzm <2000jszm@protonmail.com>
2023-08-29 22:22:35 -0500

    Se hizo seleccion de variables y se está seleccionando el mejor modelo

--- a_funciones.py
+++ a_funciones.py
@@ -1,5 +1,9 @@
 import pandas as pd
+import numpy as np
 from sklearn.impute import SimpleImputer
+from sklearn.feature_selection import SelectFromModel
+from sklearn.linear_model import Lasso
+from sklearn.model_selection import cross_val_score
 
 ### Función para conocer información de cada una de las variables del dataframe
 def info_columns(df):
@@ -15,3 +19,48 @@ def impute_columns(df, columns, strategy):
     column_imputed = imputer.fit_transform(df[column].values.reshape(-1, 1))
     df[column] = column_imputed.flatten()
   return df
+
+#Funcion de selecciond de variables
+def sel_variables(modelos,X,y,threshold):
+    
+    var_names_ac=np.array([])
+    for modelo in modelos:
+        #modelo=modelos[i]
+        modelo.fit(X,y)
+        sel = SelectFromModel(modelo, prefit=True,threshold=threshold)
+        var_names= sel.get_feature_names_out(modelo.feature_names_in_)
+        var_names_ac=np.append(var_names_ac, var_names)
+        var_names_ac=np.unique(var_names_ac)
+    
+    return var_names_ac
+
+
+#Funcion de selecciond de variables
+def sel_variablesLasso(modelos,X,y,alpha,max_features):
+    
+    var_names_ac = np.array([])
+    for modelo in modelos:
+        #modelo=modelos[i]
+        modelo.fit(X,y)
+        sel = SelectFromModel(estimator=Lasso(alpha=alpha), max_features=max_features)
+        sel.fit(X, y)
+        var_indices = sel.get_support(indices=True)
+        var_names = X.columns[var_indices]
+        var_names_ac = np.append(var_names_ac, var_names)
+        var_names_ac = np.unique(var_names_ac)
+    
+    return var_names_ac
+
+
+#Funcion para medir los modelos de regresion
+def medir_modelos(modelos,scoring,X,y,cv):
+
+    metric_modelos=pd.DataFrame()
+    for modelo in modelos:
+        scores=cross_val_score(modelo,X,y, scoring=scoring, cv=cv )
+        pdscores=pd.DataFrame(scores)
+        metric_modelos=pd.concat([metric_modelos,pdscores],axis=1)
+    
+    metric_modelos.columns=["reg_lineal","decision_tree","random_forest","gradient_boosting"]
+    return metric_modelos
+
